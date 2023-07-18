680fdbb97c77eaa16a73caca4ea49fc645bd035f
suriyanS <s.suriyanmca@gmail.com>
2023-07-18 20:06:51 +1000

    Added Code coverage badges

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    moduleDirectories: ["node_modules", "src"],
-    coverageThreshold: {
-      global: {
-        branches: 90,
-        functions: 90,
-        lines: 90,
-        statements: 90,
-      },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
-    testMatch: ["<rootDir>/__tests__/**/*.test.ts"]
-  };
+  },
+  coverageReporters: ["json-summary"],
+  testMatch: ["<rootDir>/__tests__/**/*.test.ts"],
+};
